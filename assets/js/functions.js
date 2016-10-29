d77246073000b6a816ce2bbe5e6215a249b58dc0
Karthik Meda <karthikreddymeda@gmail.com>
2016-10-29 17:18:57 +0530

    Update 2016-10-29T11:48:55.403Z

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,6 +3,7 @@
 var channelArr = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
 function getAllChannels(){
+  $('.channel-block').empty();
   channelArr.forEach(function(channel){
     $.getJSON("https://wind-bow.hyperdev.space/twitch-api/streams/"+channel+"?callback=?", function(stream){
       var channelStatus;
@@ -21,14 +22,81 @@ function getAllChannels(){
           channelLogo = channel.logo;
           channelURL = channel.url;
           var channelHTML = '<div class="channel"><div class="logo" \
-                            style="background:url('+channelLogo+') no-repeat center center; background-size: cover;">\
-                            </div><div class="stream"><a href='+channelURL+'><div class="name">'+channelName+'</div>\
-                            </a><div class="status">'+channelStatus+'</div></div></div>'
+                            style="background:url('+channelLogo+') no-repeat center center; background-size: cover;"></div>\
+                            <div class="stream"><a href='+channelURL+' target="_blank"><div class="name">'+channelName+'</div></a>\
+                            <div class="status">'+channelStatus+'</div></div></div>'
+
           $('.channel-block').append(channelHTML);
       });
     });
   });
 }
+
+function getOnlineChannels(){
+  $('.channel-block').empty();
+  channelArr.forEach(function(channel){
+    $.getJSON("https://wind-bow.hyperdev.space/twitch-api/streams/"+channel+"?callback=?", function(stream){
+      var channelStatus;
+      if (stream.stream == null){
+        channelStatus = "offline";
+      }
+      else if(stream.stream == undefined) {
+        channelStatus = "Account Closed";
+      }
+      else {
+        channelStatus = stream.stream.channel.status;
+      }
+
+      $.getJSON("https://wind-bow.hyperdev.space/twitch-api/channels/"+channel+"?callback=?", function(channel){
+          channelName = channel.display_name;
+          channelLogo = channel.logo;
+          channelURL = channel.url;
+          var channelHTML = '<div class="channel"><div class="logo" \
+                            style="background:url('+channelLogo+') no-repeat center center; background-size: cover;"></div>\
+                            <div class="stream"><a href='+channelURL+' target="_blank"><div class="name">'+channelName+'</div></a>\
+                              <div class="status">'+channelStatus+'</div></div></div>'
+        if (channelStatus != "offline"){
+          $('.channel-block').append(channelHTML);
+        }
+
+      });
+    });
+  });
+}
+
+
+function getOfflineChannels(){
+  $('.channel-block').empty();
+  channelArr.forEach(function(channel){
+    $.getJSON("https://wind-bow.hyperdev.space/twitch-api/streams/"+channel+"?callback=?", function(stream){
+      var channelStatus;
+      if (stream.stream == null){
+        channelStatus = "offline";
+      }
+      else if(stream.stream == undefined) {
+        channelStatus = "Account Closed";
+      }
+      else {
+        channelStatus = stream.stream.channel.status;
+      }
+
+      $.getJSON("https://wind-bow.hyperdev.space/twitch-api/channels/"+channel+"?callback=?", function(channel){
+          channelName = channel.display_name;
+          channelLogo = channel.logo;
+          channelURL = channel.url;
+          var channelHTML = '<div class="channel"><div class="logo" \
+                            style="background:url('+channelLogo+') no-repeat center center; background-size: cover;"></div>\
+                            <div class="stream"><a href='+channelURL+' target="_blank"><div class="name">'+channelName+'</div></a>\
+                              <div class="status">'+channelStatus+'</div></div></div>'
+        if (channelStatus === "offline"){
+          $('.channel-block').append(channelHTML);
+        }
+
+      });
+    });
+  });
+}
+
 
 $(document).ready(function(){
 
@@ -40,18 +108,14 @@ $(document).ready(function(){
   });
 
 
-  $('.online').on('click', function(){
-
-
-
+  $('.online').click(function(){
+    $('.channel-block').empty();
+    getOnlineChannels();
   });
 
-  $('.offline').on('click', function(){
-    if($('.status').val() != 'offline'){
-      $('.channel').hide();
-    }
-
-
+  $('.offline').click(function(){
+    $('.channel-block').empty();
+    getOfflineChannels();
   });
 
 
